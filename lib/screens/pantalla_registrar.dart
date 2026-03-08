e2012135274a6b46262efae38797212ba3aecf7b
Ian Novoa <iannovoacc8@gmail.com>
2026-03-07 19:38:50 -0500

    Agregando botones a la pantalla de registrar

--- lib/screens/pantalla_registrar.dart
+++ lib/screens/pantalla_registrar.dart
@@ -1,7 +1,16 @@
+// import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
-class PantallaRegistrar extends StatelessWidget {
+class PantallaRegistrar extends StatefulWidget {
   const PantallaRegistrar({super.key});
+
+  @override
+  State<PantallaRegistrar> createState() => _PantallaRegistrarState();
+}
+
+class _PantallaRegistrarState extends State<PantallaRegistrar> {
+  bool _isChecked = false;
 
   @override
   Widget build(BuildContext context) {
@@ -25,8 +34,14 @@ class PantallaRegistrar extends StatelessWidget {
         padding: EdgeInsets.all(20),
         child: Column(
           children: [
+            Icon(Icons.account_circle_outlined, size: 50, color: Colors.white),
+            SizedBox(height: 30),
             TextField(
               decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.email_outlined,
+                  color: const Color.fromRGBO(38, 101, 140, 1),
+                ),
                 border: OutlineInputBorder(),
                 hintText: 'Email',
                 contentPadding: EdgeInsets.all(15),
@@ -35,10 +50,89 @@ class PantallaRegistrar extends StatelessWidget {
             SizedBox(height: 20),
             TextField(
               decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.lock_outline_rounded,
+                  color: const Color.fromRGBO(38, 101, 140, 1),
+                ),
                 border: OutlineInputBorder(),
                 hintText: 'Contraseña',
                 contentPadding: EdgeInsets.all(15),
               ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.lock_outline_rounded,
+                  color: const Color.fromRGBO(38, 101, 140, 1),
+                ),
+                border: OutlineInputBorder(),
+                hintText: 'Confirmar Contraseña',
+                contentPadding: EdgeInsets.all(15),
+              ),
+            ),
+            SizedBox(height: 20),
+            CheckboxListTile(
+              value: _isChecked,
+              title: Text(
+                "Acepto terminos y condiciones",
+                style: TextStyle(color: Colors.white),
+              ),
+              onChanged: (bool? value) {
+                setState(() {
+                  _isChecked = value ?? false;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Acción al presionar el botón de inicio de sesión
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.blue,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 50,
+                  vertical: 15,
+                ),
+              ),
+              child: const Text('Regitrarse'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Crear cuenta con",
+              style: TextStyle(fontSize: 14, color: Colors.grey[300]),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Acción al presionar el botón de inicio de sesión
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.blue,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 50,
+                  vertical: 15,
+                ),
+              ),
+              child: const Text('Crear cuenta con Google'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Acción al presionar el botón de inicio de sesión
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.blue,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 42,
+                  vertical: 15,
+                ),
+              ),
+              child: const Text('Crear cuenta con Facebook'),
             ),
           ],
         ),
